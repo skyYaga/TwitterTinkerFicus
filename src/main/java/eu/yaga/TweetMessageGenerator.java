45cca3e29a568e9bd578d6a80485d174d80f3e05
Andreas Mueller <andreas.mueller@yaga.eu>
2016-02-14 16:24:37 +0100

    add /target to .gitignore add FicusLife Hashtag in messages

--- src/main/java/eu/yaga/TweetMessageGenerator.java
+++ src/main/java/eu/yaga/TweetMessageGenerator.java
@@ -12,17 +12,17 @@ public class TweetMessageGenerator {
 
     private static final Logger LOGGER = Logger.getLogger(TweetMessageGenerator.class.getName());
 
-    public static final String POUR_1 = "Hey @%s, I need a shower (moisture: %d)";
-    public static final String POUR_2 = "I'm so thirsty! @%s, gimme a drink! (moisture: %d)";
-    public static final String POUR_3 = "There's more water in the desert than in my soil. @%s, pour me! (moisture: %d)";
+    public static final String POUR_1 = "Hey @%s, I need a shower (moisture: %d) #FicusLife";
+    public static final String POUR_2 = "I'm so thirsty! @%s, gimme a drink! (moisture: %d) #FicusLife";
+    public static final String POUR_3 = "There's more water in the desert than in my soil. @%s, pour me! (moisture: %d) #FicusLife";
 
-    public static final String RECOVERY_1 = "I'm singing in the rain... @%s (moisture: %d)";
-    public static final String RECOVERY_2 = "Thanks for the drink @%s! Tasted like pure awesomeness (moisture: %d)";
-    public static final String RECOVERY_3 = "Damn, that shower was f*ckin' nice @%s (moisture: %d)";
+    public static final String RECOVERY_1 = "I'm singing in the rain... @%s (moisture: %d) #FicusLife";
+    public static final String RECOVERY_2 = "Thanks for the drink @%s! Tasted like pure awesomeness (moisture: %d) #FicusLife";
+    public static final String RECOVERY_3 = "Damn, that shower was f*ckin' nice @%s (moisture: %d) #FicusLife";
 
-    public static final String INFO_1 = "Yo people, what's going on? I'm feeling awesome (moisture: %d)";
-    public static final String INFO_2 = "It's time for a service tweet. My current moisture value is %d)";
-    public static final String INFO_3 = "OMG!!! What was that? Ah, nothing... (moisture: %d)";
+    public static final String INFO_1 = "Yo people, what's going on? I'm feeling awesome, enjoying #FicusLife (moisture: %d)";
+    public static final String INFO_2 = "It's time for a service tweet. My current moisture value is %d) #FicusLife";
+    public static final String INFO_3 = "OMG!!! What was that? Ah, nothing... (moisture: %d) #FicusLife";
 
     /**
      * Create a random tweet message asking the owner to pour
